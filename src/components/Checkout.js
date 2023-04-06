364276b0b9f7dce3d5fc0fc235c87aedbb177a5f
Mahamud Arteh <Artehrush@gmail.com>
2023-04-06 11:14:47 +0100

    Checkout component created

---
+++ src/components/Checkout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Checkout = () => {
+    const [basket, setBasket] = useState([]);
+    const [total, setTotal] = useState(0);
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Checkout
