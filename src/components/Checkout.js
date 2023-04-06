7a3bdee18f90ab107e2143a2e21d7cb17080148e
Mahamud Arteh <Artehrush@gmail.com>
2023-04-06 11:18:41 +0100

    basic Checkout component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,12 +4,38 @@ const Checkout = () => {
     const [basket, setBasket] = useState([]);
     const [total, setTotal] = useState(0);
 
+    const addItem = (item) => {
+        setBasket([...basket, item]);
+      };
+    
+      const removeItem = (itemIndex) => {
+        const newBasket = [...basket];
+        newBasket.splice(itemIndex, 1);
+        setBasket(newBasket);
+      };
 
+    const handleInputChange = (event) => {
+        const newItem = event.target.value.toUpperCase();
+        addItem(newItem);
+        event.target.value = '';
+      };
 
     return (
         <div>
-            
+        <h2>Checkout</h2>
+        <input type="text" placeholder="Enter item" onChange={handleInputChange} />
+        <div>
+          <h3>Basket</h3>
+          <ul>
+            {basket.map((item, index) => (
+              <li key={index}>
+                {item} <button onClick={() => removeItem(index)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+          <p>Total: {total}</p>
         </div>
+      </div>
     );
 };
 
