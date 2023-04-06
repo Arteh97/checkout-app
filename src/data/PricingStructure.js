43657276c1af962d04ebb1e5ecaa755d4f35f24e
Mahamud Arteh <Artehrush@gmail.com>
2023-04-06 11:22:13 +0100

    pricing rules implemented

---
+++ src/data/PricingStructure.js
@@ -0,0 +1,7 @@
+export const pricingRules = {
+    A: { unitPrice: 50, specialPrice: { quantity: 3, price: 130 } },
+    B: { unitPrice: 30, specialPrice: { quantity: 2, price: 45 } },
+    C: { unitPrice: 20 },
+    D: { unitPrice: 15 },
+  };
+  
